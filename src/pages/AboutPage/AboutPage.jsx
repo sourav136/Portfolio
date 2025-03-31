151c635473880e26793b56edf9f30ad38462d5fd
Sourav <souravbarua789bd@gmail.com>
2025-03-31 09:53:00 +0600

    updated the about pages

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './AboutPage.css'
 import About from '../../components/About/About';
 import Footer from '../../components/Footer/Footer';
+import Skills from './../../components/Skills/Skills';
 
 const AboutPage = () => {
     return (
         <div>
             <div className="aboutpage">
                 <About/>
+                <Skills/>
                 <Footer/>
             </div>
         </div>
